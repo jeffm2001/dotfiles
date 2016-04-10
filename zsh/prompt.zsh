d5fe5c8adaae53c4c353b57c5745f6d1c323c6bb
Jeff Mann <jmann@aflcio.org>
2016-04-10 13:15:05 -0400

    add antibody bundles

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,6 +2,12 @@
 PURE_CMD_MAX_EXEC_TIME=1
 antibody bundle sindresorhus/pure
 
+#themes to consider
+#caiogondim/bullet-train-oh-my-zsh-theme
+#zenorocha/dracula-theme
+#tonyseek/oh-my-zsh-seeker-theme
+
+
 # these two guys **MUST** be loaded last.
 antibody bundle <<EOF
 zsh-users/zsh-syntax-highlighting
