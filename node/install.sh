57aa7859222be9e64fea7b215f0df2bd6dff2a4c
Jeff Mann <jmann@aflcio.org>
2016-04-10 13:16:57 -0400

    use nvm for node

---
+++ node/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Node
+#
+# This installs some initial versions of node
+
+nvm install stable
+nvm install v4.3
+
+# install grunt
+npm install -g grunt-cli
--- node/path.zsh
+++ node/path.zsh
@@ -1,2 +1,5 @@
 #!/bin/sh
-export PATH="$PATH:/usr/local/share/npm/bin:$HOME/.npm/bin"
+# export PATH="$PATH:/usr/local/share/npm/bin:$HOME/.npm/bin"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
